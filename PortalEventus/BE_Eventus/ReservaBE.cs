9a6b3957a5820cbb3784b437c79e48a11dc24b1a
Edgar <Edgar@EDGAR-PC>
2018-04-29 22:27:41 -0500

    2do cambio

---
+++ PortalEventus/BE_Eventus/ReservaBE.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE_Eventus
+{
+    public class ReservaBE
+    {
+        public int eventoid { get; set; }
+        public string titulo { get; set; }
+        public string descripcionEvento { get; set; }
+        public int categoriaid { get; set; }
+        public int cantidadMax { get; set; }
+        public int TipoClienteId { get; set; }
+        public byte[] RutaImagen { get; set; }
+        public string descripcionCategoria { get; set; }
+
+    }
+}
