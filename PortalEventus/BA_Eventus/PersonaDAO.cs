f5efb6ea27c4fd96dac97a029fc309bc18b8453a
Graziellavl <graziellavillafuerteleon@gmail.com>
2018-05-01 12:38:00 -0500

    PersonaDAO

---
+++ PortalEventus/BA_Eventus/PersonaDAO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BA_Eventus
+{
+    class PersonaDAO
+    {
+        static readonly string cadena = ConfigurationManager.ConnectionStrings["cnEventus"].ConnectionString;
+    }
+}
--- PortalEventus/BE_Eventus/Persona.cs
+++ PortalEventus/BE_Eventus/Persona.cs
@@ -11,14 +11,21 @@ namespace BE_Eventus
         public String nombrePersona { get; set; }
         public String apePersona { get; set; }
         public int sexo { get; set; }
+        public String correo { get; set; }
         public int nacionalidad { get; set; }
         public int tipoDocumento { get; set; }
         public String nroDocumento { get; set; }
         public String telefono { get; set; }
         public String celular { get; set; }
+        public String fechaNacimiento { get; set; }
+        public int pais { get; set; }
+        public int departamento { get; set; }
+        public int provincia { get; set; }
+        public int distrito { get; set; }
     }
     class Ususario
     {
-
+        public String nombreUsuario { get; set; }
+        public String contrasena { get; set; }
     }
 }
